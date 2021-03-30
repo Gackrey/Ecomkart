d08865a188df6c3dd97df6fb0519a8974713a871
Gaurav <gauravdey625@gmail.com>
2021-03-30 12:26:31 +0530

    minor change

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,7 +4,7 @@ import { CartItem } from "./CartItem";
 export function Cart({ setroute }) {
   const { cartItems } = useCart();
   return (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
+    <div>
       {cartItems.map((product) => (
         <CartItem key={product.id} dataset={product} setroute={setroute} />
       ))}

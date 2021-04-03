ffcf0b130a4c475b82a06ee6fd1f18ba073afba5
Gaurav <gauravdey625@gmail.com>
2021-04-03 17:09:30 +0530

    Filters for Mobile devices

--- src/components/Products.js
+++ src/components/Products.js
@@ -8,10 +8,20 @@ export function Products({ setroute }) {
   const [sortState, setSortState] = useState(false);
   const [filterState, setFilterState] = useState(false);
   const [sliderState, setSliderState] = useState(false);
+  const [sortButtonClicked, setsortClickedState] = useState(false);
+  const [filterButtonClicked, setfilterClickedState] = useState(false);
   const [value, setValue] = useState(1000);
   return (
-    <div style={{ display: "grid", gridTemplateColumns: "1fr 5fr" }}>
-      <div className="filterbox">
+    <div className="productbox">
+      <div
+        className="dark_background"
+        style={
+          sortButtonClicked || filterButtonClicked
+            ? { display: "block" }
+            : { display: "none" }
+        }
+      ></div>
+      <div className="filterboxDesk">
         <h1 style={{ color: "gray" }}>Sort</h1>
         <label>
           <input
@@ -155,6 +165,201 @@ export function Products({ setroute }) {
           ></input>{" "}
           Electronics
         </label>
+      </div>
+
+      <div className="filterboxMobile">
+        <div
+          className="filterboxMobile_buttons"
+          style={
+            sortButtonClicked && filterButtonClicked
+              ? { display: "none" }
+              : { display: "flex" }
+          }
+        >
+          <div
+            className="div_filters"
+            onClick={() => {
+              setsortClickedState(true);
+              document.querySelector("body").style.overflow = "hidden";
+            }}
+          >
+            <i class="fa fa-sort"></i>Sort
+          </div>
+          <div
+            className="div_filters"
+            onClick={() => {
+              setfilterClickedState(true);
+              document.querySelector("body").style.overflow = "hidden";
+            }}
+          >
+            <i class="fa fa-filter"></i>Filter
+          </div>
+        </div>
+        <div
+          className="sortBox"
+          style={sortButtonClicked ? { display: "block" } : { display: "none" }}
+        >
+          <div className="closeButton_filter_sort">
+            <h3>SORT BY</h3>
+            <button
+              onClick={() => {
+                setsortClickedState(false);
+                document.querySelector("body").style.overflow = "scroll";
+              }}
+            >
+              X
+            </button>
+          </div>
+          <p
+            onClick={() => {
+              setSortState(true);
+              dispatch({
+                type: "HIGH_TO_LOW",
+                payload: itemsInCart,
+                check: filterState
+              });
+            }}
+          >
+            Price High to Low
+          </p>
+          <p
+            onClick={() => {
+              setSortState(true);
+              dispatch({
+                type: "LOW_TO_HIGH",
+                payload: itemsInCart,
+                check: filterState
+              });
+            }}
+          >
+            Price Low to High
+          </p>
+        </div>
+        <div
+          className="filterBox"
+          style={
+            filterButtonClicked ? { display: "block" } : { display: "none" }
+          }
+        >
+          <div className="closeButton_filter_sort">
+            <h3>FILTER BY</h3>
+            <button
+              onClick={() => {
+                setfilterClickedState(false);
+                document.querySelector("body").style.overflow = "scroll";
+              }}
+            >
+              X
+            </button>
+          </div>
+          <label>
+            <input
+              type="checkbox"
+              checked={stockChecker}
+              onChange={(e) => {
+                setFilterState(true);
+                setStockState(!stockChecker);
+                dispatch({
+                  type: "OUT_OF_STOCK",
+                  payload: itemsInCart,
+                  check: { stockChecker, deliveryChecker }
+                });
+              }}
+            ></input>{" "}
+            Include Out of Stock
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              checked={deliveryChecker}
+              onChange={(e) => {
+                setFilterState(true);
+                setDeliveryState(!deliveryChecker);
+                dispatch({
+                  type: "FAST_DELIVERY",
+                  payload: itemsInCart,
+                  check: { stockChecker, deliveryChecker }
+                });
+              }}
+            ></input>{" "}
+            Fast Delivery Only
+          </label>
+          <label style={{ display: "block", marginTop: "1rem" }}>
+            Price Range: <br />0
+            <input
+              type="range"
+              min="0"
+              max="1000"
+              value={value}
+              step="100"
+              onChange={(e) => {
+                setValue(e.target.value);
+                setSliderState(true);
+                dispatch({
+                  type: "RANGE_FILTER",
+                  payload: e.target.value
+                });
+              }}
+            ></input>
+            {value}
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="sort"
+              onChange={() => {
+                setSortState(true);
+                dispatch({
+                  type: "PRODUCT_FILTER",
+                  payload: "men clothing"
+                });
+              }}
+            ></input>
+            Men Clothing
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="sort"
+              onChange={() => {
+                setSortState(true);
+                dispatch({
+                  type: "PRODUCT_FILTER",
+                  payload: "women clothing"
+                });
+              }}
+            ></input>{" "}
+            Women Clothing
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="sort"
+              onChange={() => {
+                setSortState(true);
+                dispatch({
+                  type: "PRODUCT_FILTER",
+                  payload: "jewellery"
+                });
+              }}
+            ></input>{" "}
+            jewellery
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="sort"
+              onChange={() => {
+                setSortState(true);
+                dispatch({
+                  type: "PRODUCT_FILTER",
+                  payload: "electronics"
+                });
+              }}
+            ></input>{" "}
+            Electronics
+          </label>
+        </div>
       </div>
       <div
         style={{

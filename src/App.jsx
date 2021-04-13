0c9cc9c85f85ccc560369a6ee33d05265de31b73
Gaurav <gauravdey625@gmail.com>
2021-04-13 14:23:54 +0530

    Added new files

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import { Cart } from "./Pages/Cart";
 import { Products } from "./Pages/Products";
 import { Wishlist } from "./Pages/Wishlist";
 import { Routes, Route } from "react-router-dom";
-import { Home } from './Pages/Home'
+import  Home  from './Pages/Home'
+import SignUp from './Pages/SignUp'
+import Login from './Pages/Login'
 import "./styles.css";
 
 export default function App() {
@@ -15,6 +17,8 @@ export default function App() {
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/products" element={<Products />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/cart" element={<Cart />} />

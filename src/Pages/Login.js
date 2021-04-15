b6d0627297562e74235cc18f892aad411cb07967
Gaurav <gauravdey625@gmail.com>
2021-04-16 02:05:11 +0530

    Added login and signup page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const [showpasswordState, setPassState] = useState(false)
     return (
-        <div>
-            
+        <div className="Login-container">
+            <div className="Login-box">
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <h1>Log </h1>
+                    <h1 style={{ color: "var(--primary)" }}>IN</h1>
+                </div>
+                <div className="input-box-text">
+                    <input type="text" placeholder="Username" />
+                </div>
+                <div className="input-box-password">
+                    <input type={showpasswordState ? "text" : "password"} placeholder="Password" />
+                    <div className="password-state" onClick={() => setPassState(!showpasswordState)}>
+                        {showpasswordState ? <i class="fas fa-eye-slash"></i> : <i class="fas fa-eye"></i>}
+                    </div>
+                </div>
+                <button className="login-click-btn">Log In</button>
+                <p style={{fontWeight:"bold"}}>Dont have a account?
+                <Link to="/signup" style={{ textDecoration: "none" }}>
+                        Sign Up</Link>
+                </p>
+                <br/>
+            </div>
         </div>
     );
 }
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const [showpasswordState, setPassState] = useState(false)
     return (
-        <div>
-            
+        <div className="Login-container">
+            <div className="Login-box">
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <h1>Sign </h1>
+                    <h1 style={{ color: "var(--primary)" }}>UP</h1>
+                </div>
+                <div className="input-box-text">
+                    <input type="text" placeholder="Username" />
+                </div>
+                <div className="input-box-text">
+                    <input type="email" placeholder="Email" />
+                </div>
+                <div className="input-box-password">
+                    <input type={showpasswordState ? "text" : "password"} placeholder="Password" />
+                    <div className="password-state" onClick={() => setPassState(!showpasswordState)}>
+                        {showpasswordState ? <i class="fas fa-eye-slash"></i> : <i class="fas fa-eye"></i>}
+                    </div>
+                </div>
+                <button className="signup-click-btn">Sign Up</button>
+                <p style={{fontWeight:"bold"}}>Already a member?
+                <Link to="/login" style={{ textDecoration: "none" }}>
+                        Log In</Link>
+                </p>
+                <br/>
+            </div>
         </div>
     );
 }
